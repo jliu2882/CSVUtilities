6a6d174aae1d3afa7a094715005b62d43445fbc7
BT_1N3_32@bktech.local <BT_1N3_32@bktech.local>
2018-12-14 10:46:17 -0500

    :((

---
+++ src/sample/CSVUtilities.java
@@ -0,0 +1,69 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class CSVUtilities {
+    private ArrayList<String> CSVData;
+    private int numColumns;
+
+    //20
+    public CSVUtilities(File csv) {
+        Path pathToFile = Paths.get("src/" + csv.toString());
+
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(",");
+                this.numColumns = attributes.length;
+                for(String attribute : attributes){
+                    CSVData.add(attribute);
+                }
+
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public List<String> getColumnHeaders() {
+        List<String> header = new ArrayList<>();
+        for(int i = 0 ; i< numColumns;i++){
+            header.add(CSVData.get(i));
+        }
+        return header;
+    }
+
+    public List<String> getDataString(int column) {
+        List<String> columnData = new ArrayList<>();
+        for(int i = column-1; i< CSVData.size();i+=numColumns){
+            columnData.add(CSVData.get(i));
+        }
+        return columnData;
+    }
+
+    public List<Integer> getDataInt(int column) {
+        List<Integer> columnData = new ArrayList<>();
+        for(int i = column-1; i< CSVData.size();i+=numColumns){
+            columnData.add(Integer.parseInt(CSVData.get(i)));
+        }
+        return columnData;
+    }
+
+    public List<Double> getDataDouble(int column) {
+        List<Double> columnData = new ArrayList<>();
+        for(int i = column-1; i< CSVData.size();i+=numColumns){
+            columnData.add((double)(Integer.parseInt(CSVData.get(i))));
+        }
+        return columnData;
+    }
+}
